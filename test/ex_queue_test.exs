087a98ec33f7aa2e3479da1d8a60ede2d740d04f
Erik Nilsen <enilsen16@live.com>
2016-11-03 17:28:55 -0700

    update tests

--- test/ex_queue_test.exs
+++ test/ex_queue_test.exs
@@ -1,8 +1,24 @@
 defmodule ExQueueTest do
   use ExUnit.Case
-  doctest ExQueue.EtsHelper
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    ExQueue.create()
+    :ok
+  end
+
+  test "add a value to the queue" do
+    ExQueue.insert("hioperator")
+    assert ExQueue.EtsHelper.lookup() == [{:saved_queue, {["hioperator"], []}}]
+  end
+
+  test "pop a value from the queue" do
+    ExQueue.insert("hioperator")
+    assert ExQueue.EtsHelper.lookup() == [{:saved_queue, {["hioperator"], []}}]
+
+    ExQueue.insert("taylor-stitch")
+    assert ExQueue.EtsHelper.lookup() == [saved_queue: {["taylor-stitch"], ["hioperator"]}]
+
+    assert ExQueue.pop() == "hioperator"
+    assert ExQueue.pop() == "taylor-stitch"
   end
 end
---
+++ test/helper_test.exs
@@ -0,0 +1,4 @@
+defmodule ExQueue.HelperTest do
+  use ExUnit.Case
+  doctest ExQueue.EtsHelper
+end
