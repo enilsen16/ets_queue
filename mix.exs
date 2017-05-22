37d7baec89caeb04a606f06f29daa9be4718569d
Erik Nilsen <enilsen16@live.com>
2017-05-22 17:09:26 +1000

    update dependency

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExQueue.Mixfile do
 
   def project do
     [app: :ex_queue,
-     version: "0.4.3",
+     version: "0.4.4",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
