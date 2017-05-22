a76de8393d0d36890ee27503263fbf8d0a923c91
Erik Nilsen <enilsen16@live.com>
2017-05-22 15:42:44 +1000

    update mix file

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule ExQueue.Mixfile do
   def project do
     [app: :ex_queue,
      version: "0.1.0",
-     elixir: "~> 1.4-dev",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -16,6 +18,12 @@ defmodule ExQueue.Mixfile do
   def application do
     [applications: [:logger],
      mod: {ExQueue.Application, []}]
+  end
+
+  defp description do
+    """
+    A FIFO queue wrapped around GenServer.
+    """
   end
 
   # Dependencies can be Hex packages:
@@ -29,5 +37,16 @@ defmodule ExQueue.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :ex_queue,
+      files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Erik Nilsen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/enilsen16/ex_queue"}
+    ]
   end
 end
